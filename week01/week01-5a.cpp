f20cd6d039199374bb3cbe2681d0ef364f267cde
yiii1x <yichengzhou111@gmail.com>
2024-09-11 11:52:15 +0800

    week01

---
+++ week01/week01-5a.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	while(n>0)
+	{
+		cout << n%2;
+		n /= 2;
+	}
+	return 0;
+}
---
+++ week01/week01-6a.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int ans=0;
+        while(star>0){
+            cout<<start%2;
+            start/=2;
+        }
+        return ans;
+    }
+};
